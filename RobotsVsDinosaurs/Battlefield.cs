27cbb5100535926620667d1b1b65e97e9e5f8982
PCBakerman <72618089+PCBakerman@users.noreply.github.com>
2020-12-30 20:38:08 -0600

    Updated the Battfield class Battle method to match the Fleet and Herd classes. Fixed some scope issues with the Battlefield class as well as update some of the member methods.

--- RobotsVsDinosaurs/Battlefield.cs
+++ RobotsVsDinosaurs/Battlefield.cs
@@ -10,30 +10,29 @@ namespace RobotsVsDinosaurs
         //Member Variables
         public Fleet fleet;
         public Herd herd;
-
+        
+        //Constructor
         public Battlefield()
         {
-            //Constructor
             fleet = new Fleet();
             herd = new Herd();
         }
+        //Member Method
         public void RunGame()
         {
 
         }
+        //Member Method
         public void DisplayWelcome()
         {
 
         }
-
-
-
-
+        //Member Method
         public void Battle()
         {
-            while(fleet.robots.Count > 0 && herd.dinosaurs.Count > 0)
+            while (fleet.robotFleet.Count > 0 && herd.dinosaurHerd.Count > 0)
             {
-                if(isDinosTurn == true)
+                if (isDinosTurn == true)
                 {
                     MakeDinoAttack();
                     isDinosTurn = false;
@@ -43,31 +42,41 @@ namespace RobotsVsDinosaurs
                     MakeRoboAttack();
                     isDinosTurns = true;
                 }
-
-
-
-
-
             }
-                
-
-
-
-        }public void MakeDinoAttack(Dinosaur dinosaur)
-
-            Console.WriteLine($"Dinosaur {dinosaur.type} is ready for battle!");
+        }
+        //Member Method
+        //Dino attacks Robot 
+        public void MakeDinoAttack(Dinosaur dinosaur)
+        { 
+            Console.WriteLine($"Dinosaur {dinosaur.type} is ready to stomp!");
+            
             ShowOpponents()
-            int userChoice = Convert.ToInt32(Console.Readline());
-            robot.Attack(herd) 
-    }   public void MakeRoboAttack(Robot robot)
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            Robot.Attack(dinosaur)
+        }
+        //Member Method
+        //Robot attacks Dinosaur
+        public void MakeRoboAttack(Robot robot)
+        {
+
+        }
+        //Member Method
         public void ShowOpponents()
-    {
-        Console.WriteLine("Please pick an opponent to battle:");
-        for(int i = 0; i < fleet.robots.Count; i++)
-        { Console.WriteLine($"{i} {fleet.robots[i].name}); };
+        {
+            Console.WriteLine("Please pick an opponent to battle:");
 
-
-
-
+            //Loops through the fleet of Robots, when zero, will add a Robot to the count.
+            for (int i = 0; i < fleet.robotFleet.Count; i++)
+            {
+                Console.WriteLine($"{i} {fleet.robotFleet[i].name}");
+            }
+        }
     }
-}       public void DisplayWinners()    
+}
+
+           
+        
+
+               
+        
+    
